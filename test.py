339f2b852694230477cfded9011097dd9bc2a2d9
MechaDragonX <MechaDragonX>
2025-05-15 22:20:53 -0700

    Import en name dict, and print English names when given article instead of codes

--- test.py
+++ test.py
@@ -3,17 +3,28 @@ from mediawiki import MediaWiki
 import random
 import wikipedia
 
-mediawiki = MediaWiki()
+lang_en = {}
 
+def import_lang_en(path):
+    global lang_en
+    with open(path) as file:
+        lang_en = json.load(file)
+
+def gen_langlist_en(codelist):
+    langlist = []
+    for code in codelist:
+        langlist.append(lang_en[code])
+    return langlist
+
+
+
+# Import 'lang code: English name' dict
+import_lang_en('data/lang-en.json')
+
+mediawiki = MediaWiki()
 # List all languages and number of them for given article
 article = mediawiki.page(input())
 print(article.title)
-langlist = list(article.langlinks.keys())
+langlist = gen_langlist_en(list(article.langlinks.keys()))
 print(f'Languages: {langlist}')
 print(f'Language Count: {len(langlist)}')
-
-
-# List all articles for given language...?
-# random_lang = langlist[random.randint(0, len(langlist) - 1)]
-# print(f'{random_lang} chosen')
-# wikipedia.set_lang(random_lang)
